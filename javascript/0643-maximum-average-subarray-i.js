cccd3db0611f6d57e0189cb0d113b7a422e072bc
Matt Hensen <matthew.hensen92@gmail.com>
2024-04-21 17:17:55 -0500

    ts,js,py 643

---
+++ javascript/0643-maximum-average-subarray-i.js
@@ -0,0 +1,24 @@
+/**
+ * 643 - Maximum Average Subarray I
+ * Sliding window | Fixed size window
+ * Time: O(n) | Space: O(1)
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findMaxAverage = function (nums, k) {
+  let curr = 0;
+
+  for (let i = 0; i < k; i++) {
+    curr += nums[i];
+  }
+
+  let res = curr / k;
+
+  for (let i = k; i < nums.length; i++) {
+    curr += nums[i] - nums[i - k];
+    res = Math.max(res, curr / k);
+  }
+
+  return res;
+};
