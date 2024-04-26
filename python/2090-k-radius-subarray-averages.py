47dcd37def9cb9f96bb3f8f629e04afa6044a1da
Matt Hensen <matthew.hensen92@gmail.com>
2024-04-25 19:38:19 -0500

    ts,js,py 2090

---
+++ python/2090-k-radius-subarray-averages.py
@@ -0,0 +1,45 @@
+"""
+2090 - K Radius Subarray Averages
+Sliding Window | Fixed size window
+Time: O(n) | Space: O(1)
+
+You are given a 0-indexed array nums of n integers, and an integer k.
+
+The k-radius average for a subarray of nums centered at some index i with the radius k is the average of all elements in nums between the indices i - k and i + k (inclusive). 
+If there are less than k elements before or after the index i, then the k-radius average is -1.
+
+Build and return an array avgs of length n where avgs[i] is the k-radius average for the subarray centered at index i.
+
+The average of x elements is the sum of the x elements divided by x, using integer division. The integer division truncates toward zero, which means losing its fractional part.
+"""
+
+from typing import List
+
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        # Edge case when k = 0, its average will be itself
+        if k == 0:
+            return nums
+
+        windowSize = 2 * k + 1
+        n = len(nums)
+        avgs = [-1] * n
+
+        # Edge case when any index won't have k elements on each side
+        if windowSize > n:
+            return avgs
+
+        # First get sum of first window of nums
+        windowSum = 0
+        for i in range(windowSize):
+            windowSum += nums[i]
+
+        # The first k-radius center
+        avgs[k] = windowSum // windowSize
+
+        # Iterate from windowSize to n using sliding window method to update windowSum
+        for i in range(windowSize, n):
+            windowSum += nums[i] - nums[i - windowSize]
+            avgs[i - k] = windowSum // windowSize
+
+        return avgs
