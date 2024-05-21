5a5ec9e78b29c0a09c20c67559c54d0084b89626
Matt Hensen <matthew.hensen92@gmail.com>
2024-05-21 13:58:12 -0500

    py 3,49,104,383,771

---
+++ python/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,24 @@
+"""
+3 - Longest Substring Without Repeating Characters
+Hash map | Sliding window | Greedy
+Time: O(n) | Space: O(min(n, m))
+Where n is the length of str and m is the space of possible characters.
+
+Given a string s, find the length of the longest substring without repeating characters.
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        letters = {}
+        left = 0
+        ans = 0
+
+        for right in range(len(s)):
+            if s[right] in letters:
+                left = max(letters[s[right]], left)
+            
+            letters[s[right]] = right + 1
+            ans = max(ans, right - left + 1)
+
+        return ans
+    
---
+++ python/0049-group-anagrams.py
@@ -0,0 +1,23 @@
+"""
+49 - Group Anagrams
+Hash map | String sorting
+Time: O(n*k*log(k)) | Space: O(n*k)
+Where n is the length of strs and k is the max str length in strs.
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            groups[key].append(s)
+        
+        return groups.values()
