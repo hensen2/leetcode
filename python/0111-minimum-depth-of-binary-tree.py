9416222996d1dcca88081a063d88f5bda4e44358
Matt Hensen <matthew.hensen92@gmail.com>
2024-05-22 13:45:34 -0500

    py 100,111,112,199,515,938,1448

---
+++ python/0111-minimum-depth-of-binary-tree.py
@@ -0,0 +1,33 @@
+"""
+111 - Minimum Depth of Binary Tree
+Binary tree | DFS | Recursion
+Time: O(n) | Space: O(n)
+Space is O(n) because the tree could be a straight line.
+
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+"""
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        if not root.left:
+            return 1 + self.minDepth(root.right)
+        if not root.right:
+            return 1 + self.minDepth(root.left)
+
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
---
+++ python/0112-path-sum.py
@@ -0,0 +1,33 @@
+"""
+112 - Path Sum
+Binary tree | DFS | Recursion
+Time: O(n) | Space: O(n)
+Space is O(n) because the tree could be a straight line.
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+"""
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node, curr):
+            if not node:
+                return False
+
+            if not node.left and not node.right:
+                return (curr + node.val) == targetSum
+
+            curr += node.val
+            return dfs(node.left, curr) or dfs(node.right, curr)
+
+        return dfs(root, 0)
