e89e108855e008c3805e15839a11a24f6329370d
Matt Hensen <matthew.hensen92@gmail.com>
2024-10-16 17:35:04 -0500

    496,901 py

--- python/0104-maximum-depth-of-binary-tree.py
+++ python/0104-maximum-depth-of-binary-tree.py
@@ -1,8 +1,7 @@
 """
-104 - Maximum Depth of Binary Tree
-Binary tree | DFS | Recursion
-Time: O(n) | Space: O(n)
-Space is O(n) because the tree could be a straight line.
+104 - Maximum Depth of Binary Tree [Easy]
+Binary Tree | DFS | Recursion
+Time: O(n) | Space: O(n) because the tree could be a straight line
 
 Given the root of a binary tree, return its maximum depth.
 
@@ -11,18 +10,20 @@ A binary tree's maximum depth is the number of nodes along the longest path from
 
 from typing import Optional
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-        
+
+
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
-        l = self.maxDepth(root.left)
-        r = self.maxDepth(root.right)
-        return max(l, r) + 1
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
