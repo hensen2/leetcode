194d9a2c2d4ff95509520e1f74547498cb677f38
Matt Hensen <matthew.hensen92@gmail.com>
2024-11-05 21:20:04 -0600

    121,547 py

---
+++ python/0547-number-of-provinces.py
@@ -0,0 +1,49 @@
+"""
+547 - Number of Provinces [Medium]
+Graph | DFS
+Time: O(n^2) | Space: O(n)
+
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, 
+and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly 
+connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+"""
+
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def dfs(node):
+            for neighbor in graph[node]:
+                # the next 2 lines are needed to prevent cycles
+                if neighbor not in seen:
+                    seen.add(neighbor)
+                    dfs(neighbor)
+
+        # build the graph
+        n = len(isConnected)
+        graph = defaultdict(list)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j]:
+                    graph[i].append(j)
+                    graph[j].append(i)
+
+        seen = set()
+        ans = 0
+
+        for i in range(n):
+            if i not in seen:
+                # add all nodes of a connected component to the set
+                ans += 1
+                seen.add(i)
+                dfs(i)
+
+        return ans
