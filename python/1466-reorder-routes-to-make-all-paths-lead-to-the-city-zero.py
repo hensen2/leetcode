12f5674a3524bda8cda560512518f4a6154f68c9
Matt Hensen <matthew.hensen92@gmail.com>
2024-11-06 18:18:38 -0600

    200,841,1466,1557 py

---
+++ python/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,44 @@
+"""
+1466 - Reorder Routes to Make All Paths Lead to the City Zero [Medium]
+Graph | DFS
+Time: O(n) | Space: O(n)
+
+There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between 
+two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads 
+in one direction because they are too narrow.
+
+Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
+
+This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+
+Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+
+It's guaranteed that each city can reach city 0 after reorder.
+"""
+
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        roads = set()
+        graph = defaultdict(list)
+        for x, y in connections:
+            graph[x].append(y)
+            graph[y].append(x)
+            roads.add((x, y))
+
+        def dfs(node):
+            ans = 0
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    if (node, neighbor) in roads:
+                        ans += 1
+                    seen.add(neighbor)
+                    ans += dfs(neighbor)
+
+            return ans
+
+        seen = {0}
+        return dfs(0)
